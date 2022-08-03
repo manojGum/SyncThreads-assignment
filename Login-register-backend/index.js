fcd2c6acf487ef01d64a1bb1b54a501a51afe1f1
manojGum <Manojgum@gmail.com>
2022-08-03 13:27:19 +0530

    some change

--- Login-register-backend/index.js
+++ Login-register-backend/index.js
@@ -14,16 +14,71 @@ mongoose.connect('mongodb://localhost:27017/myLoginRegisterDB',{
 },()=>{
     console.log("DB connected")
 });
+// user Schema
+const userSchema= new mongoose.Schema({
+    name:String,
+    email: String,
+    password: String
+})
+
+// model
+const User=new mongoose.model("User",userSchema)
 
 //Routes
-// app.get("/login",(req,res)=>{
-//     res.send("My API login get")
-// })
-app.post("/login",(req,res)=>{
-    res.send("My API login")
+app.get("/",(req,res)=>{
+    res.send("My API")
 })
+
+
+
+app.post("/login",(req,res)=>{
+    const {email , password}=req.body
+    User.findOne({email:email},(err,user)=>{
+        if(user){
+            if(password===user.password){
+                res.send({message:"Login successfull",user:user})
+            }else{
+                res.send({message:"user and password didn't match"})
+            }
+        }else{
+            res.send({message:"User not Registered"})
+        }
+    })
+})
+
+
+
+
 app.post("/register",(req,res)=>{
-    res.send("My API register")
+    // // res.send("My API register")
+    // console.log(req.body)
+    const {name , email , password}=req.body
+    // check alredy exist or not 
+    User.findOne({email:email},(err,user)=>{
+        if(user){
+            res.send({message:"User already registerd"})
+        }
+        else{
+            // mongoDb user create
+    const user= new User({
+        // name:name
+        name,
+        email,
+        password
+    })
+    user.save(err=>{
+       if(err){
+        res.send(err)
+       }
+       else{
+        res.send({message:"Successfully Registerd"})
+       } 
+    })
+        }
+
+    })
+
+    
 })
 const port=8080
 app.listen(port,()=>{
